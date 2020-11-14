1bf1ec339d347ae4ef2a96967421a33934896ffe
Jaden Lee <alwaysjaden@gmail.com>
2020-11-14 00:47:34 -0800

    forcast section ready with all icons

--- asset/script/script.js
+++ asset/script/script.js
@@ -1,5 +1,10 @@
 
+// Initial array of Cities
  var cities = ["Austin", "Chicago", "New York", "Orlando", "San Francisco"];
+ 
+
+    
+ // display weather info of selected button
 
  function displayWeatherInfo() {
 
@@ -15,15 +20,10 @@
     })      
     .then(function(response) {
 
-      https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
-      
       console.log(response);
-
+        // get Selected City's Lat/Lon to get API call from other API list
       var cityLat = response.coord.lat;
       var cityLon = response.coord.lon;
-
-      console.log(cityLat);
-      console.log(cityLon);
 
       var queryURLCurrent = "https://api.openweathermap.org/data/2.5/onecall?lat=" + cityLat + "&lon=" + cityLon + "&appid=166a433c57516f51dfab1f7edaed8413";
 
@@ -33,35 +33,73 @@
         })      
         .then(function(response2) {
 
-          https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
-          
           console.log(response2);
-      
-          var cityDiv = $("<h2 class='currentCity'>").text(city);
+            // Display City + Date + Weather on Screen
+          
+          
+          var fmt1 = 'MM/DD/YYYY ';
+          let date = moment().format(fmt1);;
+          var cityDiv = $("<h2 class='currentCity'>").text(city + " ( " + date + ")");
+          var CurIconNum = (response2.current.weather[0]["icon"]);
+          console.log(CurIconNum);
+          var currentWeather = $("<img>");
+          currentWeather.attr("src","http://openweathermap.org/img/wn/"+ CurIconNum +"@2x.png");
+
+
+
+            // Get/Display Current Weather Detail
           var tempF = (response2.current.temp - 273.15) * 1.80 + 32;
           var pTemp = $("<p>").text("Temperature : " + tempF.toFixed(2) + " °F");
           var pHum = $("<p>").text("Humidity : " + response2.current.humidity + " %");
-          var pWind = $("<p>").text("Wind Speed : " + response2.current.speed + " MPH");
-          var pUV = $("<p>").text("UV Index : " + response2.current.uvi );
+          var pWind = $("<p>").text("Wind Speed : " + response2.current.wind_speed + " MPH");
+          var pUV = $('<p class="uvindex">').text("UV Index : " + response2.current.uvi );
 
+          $("#currentWeather").append(cityDiv, currentWeather, pTemp, pHum, pWind, pUV);
 
-          $("#currentWeather").append(cityDiv, pTemp, pHum, pWind, pUV);
+          if ( response2.current.uvi <= 2.9 && response2.current.uvi >= 0)
+          { $(".uvindex").attr("class","uvindex green");
+            } else if (response2.current.uvi <= 5.9 && response2.current.uvi >= 3) {
+                $(".uvindex").attr("class","uvindex yellow");
+            } else if (response2.current.uvi <= 7.9 && response2.current.uvi >= 6) {
+              $(".uvindex").attr("class","uvindex orange");
+            } else if (response2.current.uvi <= 10.9 && response2.current.uvi >= 8) {
+              $("moment().format()uvindex").attr("class","uvindex red");
+            } else { 
+              $(".uvindex").attr("class","uvindex violet");
+            }
+          
+          var forcastText = $("<h4>").text("5 Day Forcast")
+          var forcastData = $("<div class='forcast5day row'>");
+          $(".forcastSec").append(forcastText,forcastData);
 
-      
+        for (var i =0; i<5 ; i++){
+          var forcastCol = $('<div class="forcasts col-sm-2 rounded">');
+          $(".forcast5day").append(forcastCol);
+
+          // Get Date Value with moment.js
+          var fmt1 = 'MM/DD/YYYY ';
+          let future = moment().add(+[i]+1,'days');
+          var futureDate = moment(future).format(fmt1);
+
+         
+          var pDate = $("<h4>").text(futureDate);
+          var futIconNum = (response2.daily[i].weather[0]["icon"]);
+          console.log(futIconNum)
+          var pWeather = $("<img>");
+          pWeather.attr("src","http://openweathermap.org/img/wn/"+ futIconNum +"@2x.png");
+
+          var pTemp = $("<p>").text("Temp : " + tempF.toFixed(2) + " °F");
+          var pHum = $("<p>").text("Humidity : " + response2.daily[i].humidity + " %");
+          var forcastCol = document.querySelectorAll(".forcasts");
+          
+          $(forcastCol[i]).append(pDate,pWeather, pTemp, pHum);
+          
+         
+        };      
     });
-   
-    
-      // Date
-      // Image
-      // Date-temp
-      // Date-humidity
-      
-    })
-      
+ });
+}
 
-
- }
-// Initial array of Cities
 
 
 // Function for displaying movie data
